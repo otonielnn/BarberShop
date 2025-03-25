285b29a2456b4313af5f06f17bce3d3aa82005d4
otonielnn <otoniel_2002@live.com>
2025-03-25 17:36:25 -0300

    feat: página de cadastro de clientes
    Signed-off-by: otonielnn <otoniel_2002@live.com>

--- barber-shop-ui/src/app/clients/new-client/new-client.component.ts
+++ barber-shop-ui/src/app/clients/new-client/new-client.component.ts
@@ -1,18 +1,40 @@
 import { ClientsService } from '../../services/api-client/clients/clients.service';
 import { IClientService } from '../../services/api-client/clients/iclients.service';
 import { SERVICES_TOKEN } from './../../services/service.token';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { ClientModelForm } from '../client.models';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-client',
-  imports: [],
+  imports: [ClientFormComponent],
   templateUrl: './new-client.component.html',
   styleUrl: './new-client.component.scss',
   providers: [
     { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService }
   ],
 })
-export class NewClientComponent {
+export class NewClientComponent implements OnDestroy{
 
-  constructor(@Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService) {}
+  private httpSubscription?: Subscription
+
+  constructor(
+    @Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService,
+    private readonly router: Router
+  ) {}
+
+  ngOnDestroy(): void {
+    if (this.httpSubscription) {
+      this.httpSubscription.unsubscribe()
+    }
+  }
+
+  onSubmitClient(value: ClientModelForm) {
+    const { id, ...request } = value
+    this.httpSubscription = this.httpService.save(request).subscribe(_ => {
+      this.router.navigate(['clients/list'])
+    })
+  }
 }
--- barber-shop-ui/src/app/services/snackbar-manager.service.ts
+++ barber-shop-ui/src/app/services/snackbar-manager.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { ISnackbarManagerService } from './isnackbar-manager.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SnackbarManagerService {
+export class SnackbarManagerService implements ISnackbarManagerService {
 
   constructor() { }
+  show(message: string, action?: string, duration?: number): void {
+    throw new Error('Method not implemented.');
+  }
 }
