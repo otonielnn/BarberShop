28af2f34b5606bbaf93daf7dd40e0c8425dfb55b
otonielnn <otoniel_2002@live.com>
2025-03-25 00:26:52 -0300

    feat: Serviço HTTP do Client
    Signed-off-by: otonielnn <otoniel_2002@live.com>

--- barber-shop-ui/src/app/clients/new-client/new-client.component.ts
+++ barber-shop-ui/src/app/clients/new-client/new-client.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { IClientService } from '../../services/api-client/clients/iclients.service';
+import { SERVICES_TOKEN } from './../../services/service.token';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-new-client',
   imports: [],
   templateUrl: './new-client.component.html',
-  styleUrl: './new-client.component.scss'
+  styleUrl: './new-client.component.scss',
+  providers: [
+    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService }
+  ],
 })
 export class NewClientComponent {
 
+  constructor(@Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService) {}
 }
---
+++ barber-shop-ui/src/app/services/api-client/clients/client.models.ts
@@ -0,0 +1,35 @@
+export interface SaveClientRequest {
+    name: string;
+    email: string;
+    telefone: string;
+}
+
+export interface UpdateClientRequest {
+    name: string;
+    email: string;
+    telefone: string;
+}
+
+export interface SaveClientResponse {
+    name: string;
+    email: string;
+    telefone: string;
+}
+
+export interface UpdateClientResponse {
+    name: string;
+    email: string;
+    telefone: string;
+}
+
+export interface ListClientResponse {
+    name: string;
+    email: string;
+    telefone: string;
+}
+
+export interface DetailClientResponse {
+    name: string;
+    email: string;
+    telefone: string;
+}
