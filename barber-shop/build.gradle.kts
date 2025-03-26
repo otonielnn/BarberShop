7bf1769193725775622fb2a8dae7eff725c0d4de
otonielnn <otoniel_2002@live.com>
2025-03-26 05:47:56 -0300

    add: depedencias, migrations e docker-compose
    Signed-off-by: otonielnn <otoniel_2002@live.com>

--- barber-shop/build.gradle.kts
+++ barber-shop/build.gradle.kts
@@ -11,7 +11,7 @@ java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
-}
+} 
 
 configurations {
 	compileOnly {
@@ -23,17 +23,31 @@ repositories {
 	mavenCentral()
 }
 
+var mapStructVersion = "1.6.3"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
+	implementation("org.mapstruct:mapstruct:${mapStructVersion}")
+
 	compileOnly("org.projectlombok:lombok")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	runtimeOnly("org.postgresql:postgresql")
+
+	annotationProcessor("org.mapstruct:mapstruct-processor:${mapStructVersion}")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+		exclude(group = "org.flywaydb", module = "flyway-core")
+	}
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
